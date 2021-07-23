d86ab509ccd2b23f2f1b56a010a84906dbece7e7
Admiralbulldog <roopekausi@hotmail.fi>
2021-07-23 18:09:16 +0300

    c

--- main.py
+++ main.py
@@ -17,15 +17,49 @@ import torchvision.transforms as T
 
 env = gym.make('CartPole-v0').unwrapped
 
-# set up matplotlib
 is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
     from IPython import display
 
 plt.ion()
 
-# if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+
+def get_cart_location(screen_width):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+
+def get_screen():
+    # Returned screen requested by gym is 400x600x3, but is sometimes larger
+    # such as 800x1200x3. Transpose it into torch order (CHW).
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    # Cart is in the lower half, so strip off the top and bottom of the screen
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(screen_width)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0)
+
 
 class ExperienceMemory():
     def __init__(self, capacity):
@@ -33,7 +67,7 @@ class ExperienceMemory():
     def store(self, transition):
         self.memory.append(transition)
     def sample(self, batch_size=32):
-        random.sample(self.memory, batch_size)
+        return random.sample(self.memory, batch_size)
     def __len__(self):
         return len(self.memory)    
 
@@ -64,40 +98,67 @@ class DQNN(nn.Module):
         return self.head(x.view(x.shape[0], -1))
 
 
+
 class Agent():
-    def __init__(self, h, w, num_actions, epsilon=0.5, eps_min=0.05):
-        self.policy_net = DQNN(h, w, 1)
-        self.tgt_net = DQNN(h, w, 1)
-        self.epsilon = epsilon
+    def __init__(self, h, w, num_actions, eps_max=0.9, eps_min=0.05, eps_decay=200, gamma=0.9):
+        self.policy_net = DQNN(h, w, num_actions)
+        self.tgt_net = DQNN(h, w, num_actions)
+        self.eps_max = eps_max
+        self.eps_min = eps_min
+        self.eps_decay = eps_decay
+        self.gamma = gamma
         self.num_actions = num_actions
-        self.batch_size = 32
-        self.memory = ExperienceMemory(10000, self.batch_size)
+        self.batch_size = 64
+        self.tgt_update_interval = 500
+        self.memory = ExperienceMemory(10000)
+        self.optimizer = optim.RMSprop(self.policy_net.parameters())
+        self.loss_func = nn.SmoothL1Loss()
+        self.last_loss = 999
     def select_act(self, state):
         sample = random.random()
-        if sample < self.epsilon:
+        epsilon = self.eps_min + (self.eps_max-self.eps_min) * math.exp(-1. * steps_done / self.eps_decay)
+        if sample < epsilon:
             return torch.tensor(random.randrange(self.num_actions))
         else:
             with torch.no_grad():
-                return self.policy_net(state).max(0)[1]
+                return self.policy_net(state).max(1)[1].squeeze()
     def learn(self):
         if len(self.memory) < self.batch_size:
             return
         else:    
             next_states, rewards, actions, states = list(zip(*self.memory.sample(self.batch_size)))
-            next_states_stack = torch.stack(next_states, dim=0)
+            next_states_stack = torch.cat(next_states, dim=0)
             rewards_stack = torch.stack(rewards, dim=0)
             actions_stack = torch.stack(actions, dim=0)
-            states_stack = torch.stack(states, dim=0)
-            outputs = self.policy_net(states_stack).gather(1, actions_stack.view(-1,1))
-            qvalues = 
+            states_stack = torch.cat(states, dim=0)
+            q_values = self.policy_net(states_stack).gather(1, actions_stack.view(-1,1))
+            next_state_maxes = self.tgt_net(next_states_stack).max(1)[0].unsqueeze(1).detach()
+            target_q_values = rewards_stack.unsqueeze(1) + self.gamma * next_state_maxes
 
-def get_screen():
-    return 0
+            self.optimizer.zero_grad()
+            loss_func = nn.SmoothL1Loss()
+            loss = self.loss_func(q_values, target_q_values)
+            self.last_loss = loss.item()
+            loss.backward()
+            for param in self.policy_net.parameters():
+                param.grad.data.clamp_(-1, 1)
+            self.optimizer.step()
+            
+    def update_tgt_net(self):
+        self.tgt_net.load_state_dict(self.policy_net.state_dict())        
+    def get_last_loss(self):
+        return self.last_loss    
 
 
 
 episodes = 1000
-agnt = Agent(1,1,env.action_space.n)
+env.reset()
+init_screen = get_screen()
+_, _, h, w = init_screen.shape
+agnt = Agent(h,w,env.action_space.n)
+tgt_update_interval = 10
+steps_done = 0
+
 
 for e in range(episodes):
     env.reset()
@@ -106,17 +167,21 @@ for e in range(episodes):
     state = last_screen - current_screen
     for t in count():
         action = agnt.select_act(state)
-        _, reward, done, _ = env.step(action)
+        _, reward, done, _ = env.step(action.item())
         if done:
+            print(f"Episode {e} finished after {t} timesteps")
+            print(f"Loss: {agnt.last_loss}")
             break
         last_screen = current_screen
         current_screen = get_screen()
         next_state = last_screen - current_screen
+        reward = torch.tensor(reward)
         agnt.memory.store((next_state, reward, action, state))
         state = next_state
         agnt.learn()
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+        steps_done += 1
+        if t % tgt_update_interval:
+            agnt.update_tgt_net()
+        
 
 env.close()        
