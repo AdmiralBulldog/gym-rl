a68d5d0a9e8b9ccfb64d7fe10a65c540942caa73
Admiralbulldog <roopekausi@hotmail.fi>
2023-01-16 19:19:58 +0200

    add pipfile, some cleaning

--- pendulum.py
+++ pendulum.py
@@ -1,3 +1,4 @@
+
 import gym
 import math
 import random
@@ -16,7 +17,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 
-env = gym.make('BipedalWalker-v3').unwrapped
+env = gym.make('Pendulum-v0').unwrapped
 
 is_ipython = 'inline' in matplotlib.get_backend()
 if is_ipython:
@@ -26,10 +27,10 @@ plt.ion()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 episode_scores = []
 score_means = []
 fig, ax = plt.subplots()
+
 
 def plot_animate(i):
     ax.clear()
@@ -57,50 +58,47 @@ class ExperienceMemory():
 
 class DQNN_actor(nn.Module):
     def __init__(self, state_space, action_space):
+        #TODO Try batch norm to state input
+        #TODO Try wiout batchnorm
         super(DQNN_actor, self).__init__()
-        self.norm0 = nn.BatchNorm1d(state_space)
         self.lin1 = nn.Linear(state_space, 400)
-        self.norm1 = nn.BatchNorm1d(400)
         self.lin2 = nn.Linear(400,300)
-        self.norm2 = nn.BatchNorm1d(300)
         self.lin3 = nn.Linear(300, action_space)
+        nn.init.uniform_(self.lin1.weight, a=-0.003, b=0.003)
+        nn.init.uniform_(self.lin2.weight, a=-0.003, b=0.003)
         nn.init.uniform_(self.lin3.weight, a=-0.003, b=0.003)
-
+        
     def forward(self, x):
-        x = self.norm0(x.to(device))
-        x = F.relu(self.norm1(self.lin1(x)))
-        x = F.relu(self.norm2(self.lin2(x)))
-        x = torch.tanh(self.lin3(x))
-        return x
+        #x = self.norm0(x.to(device))
+        x = F.relu(self.lin1(x.to(device)))
+        x = F.relu(self.lin2(x))
+        return torch.tanh(self.lin3(x))
+        #return x*2
 
 
 class DQNN_critic(nn.Module):
     def __init__(self, state_space, action_space):
         super(DQNN_critic, self).__init__()
-        #self.lin1 = nn.Linear(state_space+action_space, 400)
-        #self.norm1 = nn.BatchNorm1d(400)
-        #self.lin2 = nn.Linear(400,300)
-        #self.norm2 = nn.BatchNorm1d(300)
-        #self.lin3 = nn.Linear(300, 1)
         self.lin1_s = nn.Linear(state_space, 400)
-        self.norm1 = nn.BatchNorm1d(400)
-        self.lin2_s = nn.Linear(400,300)
-        self.lin1_a = nn.Linear(action_space, 300)
+        self.lin2 = nn.Linear(400+action_space,300)
         self.lin3 = nn.Linear(300, 1)
+        nn.init.uniform_(self.lin1_s.weight, a=-0.003, b=0.003)
+        nn.init.uniform_(self.lin2.weight, a=-0.003, b=0.003)
         nn.init.uniform_(self.lin3.weight, a=-0.003, b=0.003)
-
+        
     def forward(self, states, actions):
+        #TODO TRY CONCAT
+        #TODO Try more batchnorm
         xs = states.to(device)
         xa = actions.to(device)
-        xs = F.relu(self.norm1(self.lin1_s(xs)))
-        xs = F.relu(self.lin2_s(xs))
-        xa = F.relu(self.lin1_a(xa))
-        x = self.lin3(torch.add(xs,xa))
-        return x
+        xs = F.relu(self.lin1_s(xs))
+        x = torch.cat((xs, xa), dim=1)
+        x = F.relu(self.lin2(x))
+        return self.lin3(x)
 
 
 class OUNoise(object):
-    def __init__(self, action_space=env.action_space, mu=0.0, theta=0.15, sigma=0.30):
+    def __init__(self, action_space=env.action_space, mu=0.0, theta=0.15, sigma=0.20):
         self.mu           = mu
         self.theta        = theta
         self.sigma        = sigma
@@ -133,7 +131,7 @@ class Agent():
         self.noise = OUNoise()
         self.tau = tau
         self.gamma = gamma
-        self.num_actions = action_space
+        self.num_actions = action_space         
         self.batch_size = batch_size
         self.memory = ExperienceMemory(memory_size)
         #self.optimizer = optim.RMSprop(self.policy_net.parameters())
@@ -158,12 +156,12 @@ class Agent():
             rewards_stack = torch.stack(rewards, dim=0)
             actions_stack = torch.stack(actions, dim=0)
             states_stack = torch.cat(states, dim=0)
+            done_stack = torch.stack(done, dim=0)
+
+            #print(f"{states_stack=}, \n {actions_stack=},\n {rewards_stack=},\n {next_states_stack=},\n {done=}")
             next_actions = self.actor_tgt(next_states_stack)
             q_values = self.critic_policy(states_stack, actions_stack)
-            target_q_values = rewards_stack.unsqueeze(1) + self.gamma * self.critic_tgt(next_states_stack, next_actions)
-            
-            for i in range(self.batch_size):
-                if done[i]: target_q_values[i] = rewards_stack[i]
+            target_q_values = rewards_stack + self.gamma * self.critic_tgt(next_states_stack, next_actions) * (1-done_stack)
                         
             self.optimizer_critic.zero_grad()
             loss_critic = self.loss_func(q_values, target_q_values)
@@ -174,10 +172,12 @@ class Agent():
             self.optimizer_critic.step()
 
             policy_actions = self.actor_policy(states_stack)
-            actions_qvalues = self.critic_tgt(states_stack, policy_actions)
+            actions_qvalues = self.critic_policy(states_stack, policy_actions)
             self.optimizer_actor.zero_grad()
 
+            #TODO CHECK IF MEAN WORKS AS EXPECTED
             loss_actor = -torch.mean(actions_qvalues)
+            
             #self.last_loss_actor = loss_actor.item()
             loss_actor.backward()
             #for param in self.actor_policy.parameters():
@@ -189,7 +189,6 @@ class Agent():
     def _update_tgt_nets(self, policy, target):
         for policy_param, target_param in zip(policy.parameters(), target.parameters()):
             target_param.data.copy_(self.tau*policy_param.data + (1.0-self.tau)*target_param.data)      
-
     def get_last_losses(self):
         return self.last_loss_critic, self.last_loss_actor
 
@@ -199,12 +198,12 @@ env.reset()
 episodes = 100000
 steps_done = 0
 
-action_space = 4
-state_space = 24
-batch_size = 100
+action_space = env.action_space.shape[0]
+state_space = 3
+batch_size = 128
 tau = 0.001
 gamma = 0.99
-memory_size = 1000000
+memory_size = 100000
 lr_actor = 0.0001
 lr_critic = 0.001
 agnt = Agent(state_space,action_space, gamma, memory_size, lr_actor, lr_critic, tau, batch_size)
@@ -212,8 +211,10 @@ done_reward = -100
 
 action_array = np.zeros((4,10))
 wiggling_tracker = [0]*4
+            
 
-
+# Seen mistakes: wrong shape in replay buffer
+#                should use policy critic when learning actor?
 with mlflow.start_run():
     mlflow.log_param("memory_size", memory_size)
     mlflow.log_param("tau", tau)
@@ -229,57 +230,42 @@ with mlflow.start_run():
     mlflow.log_param("batch_size", batch_size)
     mlflow.log_param("theta", agnt.noise.theta)
     mlflow.log_param("sigma", agnt.noise.sigma)
-
+    
     for e in range(episodes):
         agnt.noise.reset()
         state = env.reset()
         state = torch.tensor(state, dtype=torch.float, device=device).unsqueeze(0)
         episode_reward = 0
-        for t in range(700):
+        
+        for t in range(300):
             env.render()
-            action = agnt.select_act(state).cpu()
-            action = agnt.noise.get_action(np.array(action))        
-            next_state, reward, done, _ = env.step(action)
-            for i in range(len(action_array)):
-                action_array[i] = np.append(action_array[i][1:], action[i])
-            for i in range(len(action_array)):
-                actions = action_array[i]
-                wiggling_tracker[i] = np.count_nonzero(actions[1:] * actions[:-1] < 0)
-            
-            # punish for wiggling and not moving
-            #reward -= sum(wiggling_tracker)*0.5
-            reward -= np.count_nonzero(abs(action) < 0.01)*10
-            #reward += next_state[2]
+            action = agnt.select_act(state).cpu()#.data.numpy()
+            #print(f"{np.array(action)=}")   
+            action = agnt.noise.get_action(np.array(action))     
+            next_state, reward, done, _ = env.step(np.array(action))
             episode_reward += reward
             
             next_state = torch.tensor(next_state, dtype=torch.float, device=device).unsqueeze(0)
-            reward = torch.tensor(reward, dtype=torch.float, device=device)
+            reward = torch.tensor(reward, dtype=torch.float, device=device).unsqueeze(0)
+            done = torch.tensor(done, dtype=torch.float, device=device).unsqueeze(0)
+                
+            agnt.memory.store((next_state, reward, torch.tensor(action, dtype=torch.float, device=device), state, done))
             
-            # If reward is positive, add it to replaybuffer 10 times
-            if reward > 0:
-                for _ in range(10):
-                    agnt.memory.store((next_state, reward, torch.tensor(action, dtype=torch.float, device=device), state, done))
-            else:
-                agnt.memory.store((next_state, reward, torch.tensor(action, dtype=torch.float, device=device), state, done))
-
-            if done:
-                break
-
             state = next_state
             agnt.learn()
             steps_done += 1
 
+            if done:
+                break
+
         episode_scores.append(episode_reward)
         score_means.append(sum(episode_scores[-100:]) / 100)
         last_loss_critic, last_loss_actor = agnt.get_last_losses()
-
         print(f"Episode {e} finished after {t} timesteps, loss critic: {last_loss_critic}, {last_loss_actor}")
-
-        mlflow.log_metric("steps_done", steps_done)
-        mlflow.log_metric("episodes done", e)
-        mlflow.log_metric("last_avg_score", score_means[-1])
-        mlflow.log_metric("last_score", episode_scores[-1])
         
-            
-
+    mlflow.log_metric("steps_done", steps_done)
+    mlflow.log_metric("episodes done", e)
+    mlflow.log_metric("last_avg_score", score_means[-1])
+    mlflow.log_metric("last_score", episode_scores[-1])
+    
 env.close()        
--- walkerCopy.py
+++ walkerCopy.py
@@ -1,3 +1,4 @@
+
 import gym
 import math
 import random
@@ -27,8 +28,6 @@ plt.ion()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-
-
 episode_scores = []
 score_means = []
 fig, ax = plt.subplots()
@@ -45,6 +44,7 @@ def plot_animate(i):
 
 ani = animation.FuncAnimation(fig, plot_animate, interval=1000)
 
+
 class ExperienceMemory():
     def __init__(self, capacity):
         self.memory = deque([], maxlen=capacity)
@@ -55,56 +55,50 @@ class ExperienceMemory():
     def __len__(self):
         return len(self.memory)    
 
+
 class DQNN_actor(nn.Module):
     def __init__(self, state_space, action_space):
         #Try batch norm to state input
         #Try wiout batchnorm
         super(DQNN_actor, self).__init__()
-        self.norm0 = nn.BatchNorm1d(state_space)
         self.lin1 = nn.Linear(state_space, 400)
-        self.norm1 = nn.BatchNorm1d(400)
         self.lin2 = nn.Linear(400,300)
-        self.norm2 = nn.BatchNorm1d(300)
         self.lin3 = nn.Linear(300, action_space)
+        nn.init.uniform_(self.lin1.weight, a=-0.003, b=0.003)
+        nn.init.uniform_(self.lin2.weight, a=-0.003, b=0.003)
         nn.init.uniform_(self.lin3.weight, a=-0.003, b=0.003)
-
+        
     def forward(self, x):
-        x = self.norm0(x.to(device))
-        x = F.relu(self.norm1(self.lin1(x)))
-        x = F.relu(self.norm2(self.lin2(x)))
-        x = torch.tanh(self.lin3(x))
-        return x
+        #x = self.norm0(x.to(device))
+        x = F.relu(self.lin1(x.to(device)))
+        x = F.relu(self.lin2(x))
+        return torch.tanh(self.lin3(x))
+        #return x*2
+
 
 class DQNN_critic(nn.Module):
     def __init__(self, state_space, action_space):
         super(DQNN_critic, self).__init__()
-        #self.lin1 = nn.Linear(state_space+action_space, 400)
-        #self.norm1 = nn.BatchNorm1d(400)
-        #self.lin2 = nn.Linear(400,300)
-        #self.norm2 = nn.BatchNorm1d(300)
-        #self.lin3 = nn.Linear(300, 1)
         self.lin1_s = nn.Linear(state_space, 400)
-        self.norm1 = nn.BatchNorm1d(400)
-        self.lin2_s = nn.Linear(400,300)
-        self.lin1_a = nn.Linear(action_space, 300)
+        self.lin2 = nn.Linear(400+action_space,300)
         self.lin3 = nn.Linear(300, 1)
+        nn.init.uniform_(self.lin1_s.weight, a=-0.003, b=0.003)
+        nn.init.uniform_(self.lin2.weight, a=-0.003, b=0.003)
         nn.init.uniform_(self.lin3.weight, a=-0.003, b=0.003)
-
-
+        
     def forward(self, states, actions):
         #TRY CONCAT
         #Try more batchnorm
         xs = states.to(device)
         xa = actions.to(device)
-        xs = F.relu(self.norm1(self.lin1_s(xs)))
-        xs = F.relu(self.lin2_s(xs))
-        xa = F.relu(self.lin1_a(xa))
-        x = self.lin3(torch.add(xs,xa))
-        return x
+        xs = F.relu(self.lin1_s(xs))
+        x = torch.cat((xs, xa), dim=1)
+        x = F.relu(self.lin2(x))
+        return self.lin3(x)
 
 
 class OUNoise(object):
-    def __init__(self, action_space=env.action_space, mu=0.0, theta=0.15, sigma=0.30):
+    def __init__(self, action_space=env.action_space, mu=0.0, theta=0.15, sigma=0.20):
         self.mu           = mu
         self.theta        = theta
         self.sigma        = sigma
@@ -136,7 +130,7 @@ class Agent():
         self.noise = OUNoise()
         self.tau = tau
         self.gamma = gamma
-        self.num_actions = action_space
+        self.num_actions = action_space         
         self.batch_size = batch_size
         self.memory = ExperienceMemory(memory_size)
         #self.optimizer = optim.RMSprop(self.policy_net.parameters())
@@ -145,10 +139,12 @@ class Agent():
         self.loss_func = nn.MSELoss()
         self.last_loss_critic = 999
         self.last_loss_actor = 999
+
     def select_act(self, state):
         self.actor_policy.eval()
         with torch.no_grad():
             return self.actor_policy(state).squeeze()
+
     def learn(self):
         if len(self.memory) < self.batch_size:
             return
@@ -159,12 +155,11 @@ class Agent():
             rewards_stack = torch.stack(rewards, dim=0)
             actions_stack = torch.stack(actions, dim=0)
             states_stack = torch.cat(states, dim=0)
+            done_stack = torch.stack(done, dim=0)
+            #print(f"{states_stack=}, \n {actions_stack=},\n {rewards_stack=},\n {next_states_stack=},\n {done=}")
             next_actions = self.actor_tgt(next_states_stack)
             q_values = self.critic_policy(states_stack, actions_stack)
-            target_q_values = rewards_stack.unsqueeze(1) + self.gamma * self.critic_tgt(next_states_stack, next_actions)
-            
-            for i in range(self.batch_size):
-                if done[i]: target_q_values[i] = rewards_stack[i]
+            target_q_values = rewards_stack + self.gamma * self.critic_tgt(next_states_stack, next_actions) * (1-done_stack)
                         
             self.optimizer_critic.zero_grad()
             loss_critic = self.loss_func(q_values, target_q_values)
@@ -175,7 +170,7 @@ class Agent():
             self.optimizer_critic.step()
 
             policy_actions = self.actor_policy(states_stack)
-            actions_qvalues = self.critic_tgt(states_stack, policy_actions)
+            actions_qvalues = self.critic_policy(states_stack, policy_actions)
             self.optimizer_actor.zero_grad()
             #CHECK IF MEAN WORKS AS EXPECTED
             loss_actor = -torch.mean(actions_qvalues)
@@ -189,10 +184,10 @@ class Agent():
             
     def _update_tgt_nets(self, policy, target):
         for policy_param, target_param in zip(policy.parameters(), target.parameters()):
-            target_param.data.copy_(self.tau*policy_param.data + (1.0-self.tau)*target_param.data)      
+            target_param.data.copy_(self.tau*policy_param.data + (1.0-self.tau)*target_param.data)   
+
     def get_last_losses(self):
         return self.last_loss_critic, self.last_loss_actor
-
 
 
 env.reset()
@@ -200,16 +195,23 @@ env.reset()
 episodes = 100000
 steps_done = 0
 
-action_space = 4
+action_space = env.action_space.shape[0]
 state_space = 24
-batch_size = 100
+batch_size = 128
 tau = 0.001
 gamma = 0.99
-memory_size = 1000000
+memory_size = 100000
 lr_actor = 0.0001
 lr_critic = 0.001
 agnt = Agent(state_space,action_space, gamma, memory_size, lr_actor, lr_critic, tau, batch_size)
 done_reward = -100
+
+action_array = np.zeros((4,10))
+wiggling_tracker = [0]*4
+            
+
+# Seen mistakes: wrong shape in replay buffer
+#                should use policy critic when learning actor?
 with mlflow.start_run():
     mlflow.log_param("memory_size", memory_size)
     mlflow.log_param("tau", tau)
@@ -225,47 +227,44 @@ with mlflow.start_run():
     mlflow.log_param("batch_size", batch_size)
     mlflow.log_param("theta", agnt.noise.theta)
     mlflow.log_param("sigma", agnt.noise.sigma)
-    
 
     for e in range(episodes):
         agnt.noise.reset()
         state = env.reset()
         state = torch.tensor(state, dtype=torch.float, device=device).unsqueeze(0)
         episode_reward = 0
+        
         for t in range(700):
             env.render()
-            action = agnt.select_act(state).cpu()
-            action = agnt.noise.get_action(np.array(action))
-            next_state, reward, done, _ = env.step(action)
-            #reward += next_state[2]
+            action = agnt.select_act(state).cpu()#.data.numpy()
+            #print(f"{np.array(action)=}")   
+            action = agnt.noise.get_action(np.array(action))     
+            next_state, reward, done, _ = env.step(np.array(action))
             episode_reward += reward
             
-            
             next_state = torch.tensor(next_state, dtype=torch.float, device=device).unsqueeze(0)
-            reward = torch.tensor(reward, dtype=torch.float, device=device)
-
-            print(f"{action=}")
+            reward = torch.tensor(reward, dtype=torch.float, device=device).unsqueeze(0)
+            done = torch.tensor(done, dtype=torch.float, device=device).unsqueeze(0)
+                
+            agnt.memory.store((next_state, reward, torch.tensor(action, dtype=torch.float, device=device), state, done))
             
-            # If reward is positive, add it to replaybuffer 10 times
-            if reward > 0:
-                for _ in range(10):
-                    agnt.memory.store((next_state, reward, torch.tensor(action, dtype=torch.float, device=device), state, done))
-            else:
-                agnt.memory.store((next_state, reward, torch.tensor(action, dtype=torch.float, device=device), state, done))
-            if done:
-                break
             state = next_state
             agnt.learn()
             steps_done += 1
+
+            if done:
+                break
+
         episode_scores.append(episode_reward)
         score_means.append(sum(episode_scores[-100:]) / 100)
         last_loss_critic, last_loss_actor = agnt.get_last_losses()
+
         print(f"Episode {e} finished after {t} timesteps, loss critic: {last_loss_critic}, {last_loss_actor}")
-        mlflow.log_metric("steps_done", steps_done)
-        mlflow.log_metric("episodes done", e)
-        mlflow.log_metric("last_avg_score", score_means[-1])
-        mlflow.log_metric("last_score", episode_scores[-1])
-        
-            
+
+    mlflow.log_metric("steps_done", steps_done)
+    mlflow.log_metric("episodes done", e)
+    mlflow.log_metric("last_avg_score", score_means[-1])
+    mlflow.log_metric("last_score", episode_scores[-1])
+    
 
 env.close()        
